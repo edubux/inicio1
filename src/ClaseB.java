8f95457aec5eea274add94f0200cef82e7530364
Fernando <a20162001@pucp.edu.pe>
2020-09-12 16:47:04 -0500

    fer

--- src/ClaseB.java
+++ src/ClaseB.java
@@ -1,3 +1,21 @@
 public class ClaseB {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    private Integer edad;
 }
