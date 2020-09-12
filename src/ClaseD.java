8ffa5f109ac849b8d5c96012b6e18e964c74d2d9
Edux <edu,paniagua@pucp.edu.pe>
2020-09-12 17:00:12 -0500

    cambio CLassA

--- src/ClaseD.java
+++ src/ClaseD.java
@@ -1,2 +1,23 @@
 public class ClaseD {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    private String  string;
+
 }
